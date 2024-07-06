35e804f0b1a983c6caeea55fae9ef8b83b433787
shibu666 <117596410+shibu666@users.noreply.github.com>
2024-07-06 14:42:40 +0530

    changes after dockerisation

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+// client/src/index.js
+
+import React from 'react';
+import './index.css';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
